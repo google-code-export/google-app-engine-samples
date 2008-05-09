30c0f10241c7ebd3806fd127f5105826e5bacb51
ryanb+appengine@google.com <ryanb+appengine@google.com@99225164-8649-0410-878b-2ba91e509939>
2008-05-09 01:39:17 +0000

    making the list of recent logins pretty and usable. still in progress.

--- openid-consumer/consumer.py
+++ openid-consumer/consumer.py
@@ -40,15 +40,11 @@ Then go to http://localhost:8080/ in your browser, type in
 http://localhost:8000/test as your OpenID identifier, and click Verify.
 """
 
-import cgi
-import Cookie
 import datetime
 import logging
 import os
-import pickle
-import pprint
+import re
 import sys
-import traceback
 import urlparse
 import wsgiref.handlers
 
@@ -70,28 +66,36 @@ _DEBUG = False
 class Login(db.Model):
   """A completed OpenID login."""
   status = db.StringProperty(choices=('success', 'cancel', 'failure'))
-  openid = db.LinkProperty()
-  server = db.LinkProperty()
+  claimed_id = db.LinkProperty()
+  server_url = db.LinkProperty()
   timestamp = db.DateTimeProperty(auto_now_add=True)
 
 
 class Session(db.Expando):
   """An in-progress OpenID login."""
-  pass
+  claimed_id = db.StringProperty()
+  server_url = db.LinkProperty()
+  store_and_display = db.BooleanProperty()
 
 
 class Handler(webapp.RequestHandler):
   """A base handler class with a couple OpenID-specific utilities."""
   consumer = None
   session = None
+  session_args = None
+
+  
+
+  def __init__(self):
+    self.session_args = {}
 
   def get_consumer(self):
     """Returns a Consumer instance.
     """
     if not self.consumer:
       fetchers.setDefaultFetcher(fetcher.UrlfetchFetcher())
-      session = self.get_session()
-      self.consumer = Consumer(vars(session), store.DatastoreStore())
+      self.load_session()
+      self.consumer = Consumer(self.session_args, store.DatastoreStore())
 
     return self.consumer
 
@@ -104,8 +108,8 @@ class Handler(webapp.RequestHandler):
     req = self.request
     return dict([(arg, req.get(arg)) for arg in req.arguments()])
 
-  def get_session(self):
-    """Gets the current session.
+  def load_session(self):
+    """Loads the current session.
     """
     if not self.session:
       id = self.request.get('session_id')
@@ -114,14 +118,26 @@ class Handler(webapp.RequestHandler):
           self.session = db.get(db.Key.from_path('Session', int(id)))
           assert self.session
         except (AssertionError, db.Error), e:
-          self.report_error(str('Invalid session id: %d' % id))
+          self.render({'error': 'Invalid session id: %d' % id})
+
+        fields = self.session.dynamic_properties()
+        self.session_args = dict((f, getattr(self.session, f)) for f in fields)
+
       else:
+        self.session_args = {}
         self.session = Session()
+        self.session.claimed_id = self.request.get('openid')
 
-    return self.session
+  def store_session(self):
+    """Stores the current session.
+    """
+    assert self.session
+    for field, value in self.session_args.items():
+      setattr(self.session, field, value)
+    self.session.put()
 
-  def render(self, template_name, extra_values={}):
-    """render the given template, including the extra (optional) values.
+  def render(self, extra_values={}):
+    """Renders the page, including the extra (optional) values.
 
     Args:
       template_name: string
@@ -130,18 +146,20 @@ class Handler(webapp.RequestHandler):
       extra_values: dict
       Template values to provide to the template.
     """
-    query = Login.gql('ORDER BY timestamp DESC')
+    logins = Login.gql('ORDER BY timestamp DESC').fetch(20)
+    for login in logins:
+      login.friendly_time = self.relative_time(login.timestamp)
 
     values = {
       'response': {},
       'openid': '',
-      'logins': query.fetch(20),
+      'logins': logins,
       'request': self.request,
       'debug': self.request.get('deb'),
     }
     values.update(extra_values)
     cwd = os.path.dirname(__file__)
-    path = os.path.join(cwd, 'templates', template_name + '.html')
+    path = os.path.join(cwd, 'templates', 'base.html')
     logging.debug(path)
     self.response.out.write(template.render(path, values, debug=_DEBUG))
 
@@ -152,9 +170,7 @@ class Handler(webapp.RequestHandler):
       message: string
       A detailed error message.
     """
-    args = pprint.pformat(self.args_to_dict())
-    self.render('error', vars())
-    logging.error(message)
+    self.render({'error': message})
 
   def show_front_page(self):
     """Do an internal (non-302) redirect to the front page.
@@ -166,11 +182,100 @@ class Handler(webapp.RequestHandler):
     front_page.response = self.response
     front_page.get()
 
+  def relative_time(self, timestamp):
+    """Returns a friendly string describing how long ago the timestamp was.
+
+    Args:
+      timestamp: a datetime
+
+    Returns:
+      string
+    """
+    delta = datetime.datetime.now() - timestamp
+    minutes = delta.seconds / 60
+    hours = minutes / 60
+
+    if delta.days > 1:
+      return '%d days ago' % delta.days
+    elif delta.days == 1:
+      return 'yesterday'
+    elif hours > 1:
+      return '%d hours ago' % hours
+    elif hours == 1:
+      return 'an hour ago'
+    elif minutes > 25:
+      return 'half an hour ago'
+    elif minutes > 5:
+      return '%d minutes ago' % minutes
+    else:
+      return 'moments ago'
+
+
+  def display_name(self, openid_url):
+    """Extracts a short, representative part of an OpenID URL for display.
+
+    For example, it returns "ryan" for:
+      ryan.com
+      www.ryan.com
+      ryan.provider.com
+      provider.com/ryan
+      provider.com/id/path/ryan
+
+    Adapted from Net::OpenID::Consumer, by Brad Fitzpatrick. See:
+
+    http://code.sixapart.com/svn/openid/trunk/perl/Net-OpenID-Consumer/lib/Net/OpenID/VerifiedIdentity.pm
+
+    Args:
+      openid_url: string
+
+    Returns:
+      string
+    """
+    username_re = '[\w.+-]+'
+    common_tlds = ('com', 'org', 'net', 'edu', 'info', 'biz', 'gov', 'mil', 'us')
+
+    parts = urlparse.urlparse(openid_url)
+
+    def sanitize(display_name):
+      if '@' in display_name:
+        # don't display full email addresses; use just the user name part
+        display_name = display_name[:display_name.index('@')]
+      return display_name
+
+    # is the username in the params?
+    match = re.search('(u|id|user|userid|user_id|profile)=(%s)' % username_re,
+                      parts.path)
+    if match:
+      return sanitize(match.group(2))
+
+    # is the username in the path?
+    path = parts.path[1:]
+    if re.match(username_re, path):
+      return sanitize(path)
+
+    # use the hostname
+    host = parts.hostname
+    host_parts = host.split('.')
+
+    # strip the tld if it's not interesting
+    for tld in tlds_to_strip:
+      if host.endswith(tld):
+        host = host[:-(len(tld))]
+      elif host[-6:-2] == '.co.'
+        host = host[:-6]
+
+    if len(host_parts) >= 3 && host[0] != 'www':
+      return sanitize('.'.join(host_parts[:-2]
+      host = host[4:]
+
+    $host =~ s/:.+//;
+    $host =~ s/^www\.//i;
+
 
 class FrontPage(Handler):
-  """Show the default OpenID page, with the last 10 logins for this user."""
+  """Show the default front page."""
   def get(self):
-    self.render('index')
+    self.render()
 
 
 class LoginHandler(Handler):
@@ -178,6 +283,7 @@ class LoginHandler(Handler):
 
   def post(self):
     """Handles login requests."""
+    logging.info(self.args_to_dict())
     openid_url = self.request.get('openid')
     if not openid_url:
       self.show_front_page()
@@ -185,10 +291,16 @@ class LoginHandler(Handler):
     try:
       auth_request = self.get_consumer().begin(openid_url)
     except discover.DiscoveryFailure, e:
-      self.report_error(str(e))
+      self.report_error('Error during OpenID provider discovery.')
+      return
+    except discover.XRDSError, e:
+      self.report_error('Error parsing XRDS from provider.')
+      return
 
-    session = Session()
-    session.put()
+    self.session.claimed_id = auth_request.endpoint.claimed_id
+    self.session.server_url = auth_request.endpoint.server_url
+    self.session.store_and_display = self.request.get('display', 'no') != 'no'
+    self.store_session()
 
     sreg_request = sreg.SRegRequest(optional=['nickname', 'fullname', 'email'])
     auth_request.addExtension(sreg_request)
@@ -201,7 +313,7 @@ class LoginHandler(Handler):
 
     parts = list(urlparse.urlparse(self.request.uri))
     parts[2] = 'finish'
-    parts[4] = 'session_id=%d' % session.key().id()
+    parts[4] = 'session_id=%d' % self.session.key().id()
     parts[5] = ''
     return_to = urlparse.urlunparse(parts)
     realm = urlparse.urlunparse(parts[0:2] + [''] * 4)
@@ -218,25 +330,24 @@ class FinishHandler(Handler):
     response = self.get_consumer().complete(args, self.request.uri)
     assert response.status in Login.status.choices
 
+    self.load_session()
+    logging.info(self.session_args)
+
     if (response.status == 'success'):
       sreg_data = sreg.SRegResponse.fromSuccessResponse(response).items()
-#     sreg_keys = sreg_data.keys()
-#     sreg_values = sreg_data.values()
       pape_data = pape.Response.fromSuccessResponse(response)
+      self.session.claimed_id = response.endpoint.claimed_id
+      self.session.server_url = response.endpoint.server_url
 
-#     print >> sys.stderr, response.status
     login = Login(status=response.status,
-                  openid=response.endpoint.claimed_id,
-                  server=response.endpoint.server_url)
+                  claimed_id=self.session.claimed_id,
+                  server_url=self.session.server_url)
     login.put()
 
-    self.render('response', locals())
-#     self.response.out.write(
-#       '\r\n\r\n%s\n%s\r\n\r\n' % (response.status, response.message))
+    self.render(locals())
 
 
-
-# Map URLs to our RequestHandler classes above
+# Map URLs to our RequestHandler subclasses above
 _URLS = [
   ('/', FrontPage),
   ('/login', LoginHandler),
@@ -249,4 +360,3 @@ def main(argv):
 
 if __name__ == '__main__':
   main(sys.argv)
- 
