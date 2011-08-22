f9a7e5186cce39b4ee70fc7ac54ca8a3c50cf277
fredsa@google.com <fredsa@google.com@99225164-8649-0410-878b-2ba91e509939>
2011-08-22 20:56:15 +0000

    Update samples to be consistent with sharded counter article: http://code.google.com/appengine/articles/sharding_counters.html

--- sharded-counters-java/src/com/google/appengine/demos/shardedcounter/java/v1/ShardedCounter.java
+++ sharded-counters-java/src/com/google/appengine/demos/shardedcounter/java/v1/ShardedCounter.java
@@ -1,3 +1,20 @@
+/* Copyright (c) 2011 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.appengine.demos.shardedcounter.java.v1;
+
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
@@ -15,7 +32,7 @@ import java.util.Random;
  * number of shards is to add another shard by creating another entity in the
  * datastore.
  */
-public class ShardedCounterV1 {
+public class ShardedCounter {
 
   private static final DatastoreService ds = DatastoreServiceFactory
       .getDatastoreService();
@@ -26,13 +43,13 @@ public class ShardedCounterV1 {
   private static final int NUM_SHARDS = 20;
 
   /**
-   * A random number generating, for distributing writes across shards.
+   * A random number generator, for distributing writes across shards.
    */
-  private Random generator = new Random();
+  private final Random generator = new Random();
 
   /**
    * Retrieve the value of this sharded counter.
-   *
+   * 
    * @return Summed total of all shards' counts
    */
   public long getCount() {
--- sharded-counters-java/src/com/google/appengine/demos/shardedcounter/java/v2/ShardedCounter.java
+++ sharded-counters-java/src/com/google/appengine/demos/shardedcounter/java/v2/ShardedCounter.java
@@ -1,3 +1,20 @@
+/* Copyright (c) 2011 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.appengine.demos.shardedcounter.java.v2;
+
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
@@ -15,23 +32,48 @@ import java.util.Random;
 
 /**
  * A counter which can be incremented rapidly.
- *
+ * 
  * Capable of incrementing the counter and increasing the number of shards. When
  * incrementing, a random shard is selected to prevent a single shard from being
- * written to too frequently. If increments are being made too quickly, increase
+ * written too frequently. If increments are being made too quickly, increase
  * the number of shards to divide the load. Performs datastore operations using
  * the low level datastore API.
  */
-public class ShardedCounterV2 {
+public class ShardedCounter {
 
-  interface Counter {
-    static final String KIND = "Counter";
-    static final String SHARD_COUNT = "shard_count";
+  /**
+   * Convenience class which contains constants related to a named sharded
+   * counter. The counter name provided in the constructor is used as
+   * the entity key.
+   */
+  private static final class Counter {
+    /**
+     * Entity kind representing a named sharded counter.
+     */
+    private static final String KIND = "Counter";
+
+    /**
+     * Property to store the number of shards in a given {@value #KIND} named
+     * sharded counter.
+     */
+    private static final String SHARD_COUNT = "shard_count";
   }
 
-  interface CounterShard {
-    static final String COUNT = "count";
-    static final String KIND = "CounterShard";
+  /**
+   * Convenience class which contains constants related to the counter shards.
+   * The shard number (as a String) is used as the entity key.
+   */
+  private static final class CounterShard {
+    /**
+     * Entity kind prefix, which is concatenated with the counter name to form
+     * the final entity kind, which represents counter shards.
+     */
+    private static final String KIND_PREFIX = "CounterShard_";
+
+    /**
+     * Property to store the current count within a counter shard.
+     */
+    private static final String COUNT = "count";
   }
 
   private static final DatastoreService ds = DatastoreServiceFactory
@@ -40,34 +82,41 @@ public class ShardedCounterV2 {
   /**
    * Default number of shards.
    */
-  private static final long INITIAL_SHARDS = 5;
+  private static final int INITIAL_SHARDS = 5;
 
   /**
    * The name of this counter.
    */
-  private String counterName;
+  private final String counterName;
 
   /**
    * A random number generating, for distributing writes across shards.
    */
-  private Random generator = new Random();
+  private final Random generator = new Random();
 
   /**
    * The counter shard kind for this counter.
    */
   private String kind;
 
-  private MemcacheService mc = MemcacheServiceFactory.getMemcacheService();
+  private final MemcacheService mc = MemcacheServiceFactory
+      .getMemcacheService();
 
-  public ShardedCounterV2(String counterName) {
+  /**
+   * Constructor which creates a sharded counter using the provided counter
+   * name.
+   * 
+   * @param counterName name of the sharded counter
+   */
+  public ShardedCounter(String counterName) {
     this.counterName = counterName;
-    kind = CounterShard.KIND + "_" + counterName;
+    kind = CounterShard.KIND_PREFIX + counterName;
   }
 
   /**
    * Increase the number of shards for a given sharded counter. Will never
    * decrease the number of shards.
-   *
+   * 
    * @param count Number of new shards to build and store
    */
   public void addShards(int count) {
@@ -78,7 +127,7 @@ public class ShardedCounterV2 {
 
   /**
    * Retrieve the value of this sharded counter.
-   *
+   * 
    * @return Summed total of all shards' counts
    */
   public long getCount() {
@@ -103,7 +152,7 @@ public class ShardedCounterV2 {
    */
   public void increment() {
     // Find how many shards are in this counter.
-    int numShards = (int) getShardCount();
+    int numShards = getShardCount();
 
     // Choose the shard randomly from the available shards.
     long shardNum = generator.nextInt(numShards);
@@ -115,14 +164,15 @@ public class ShardedCounterV2 {
 
   /**
    * Get the number of shards in this counter.
-   *
+   * 
    * @return shard count
    */
-  private long getShardCount() {
+  private int getShardCount() {
     try {
       Key counterKey = KeyFactory.createKey(Counter.KIND, counterName);
       Entity counter = ds.get(counterKey);
-      return (Long) counter.getProperty(Counter.SHARD_COUNT);
+      Long shardCount = (Long) counter.getProperty(Counter.SHARD_COUNT);
+      return shardCount.intValue();
     } catch (EntityNotFoundException ignore) {
       return INITIAL_SHARDS;
     }
@@ -132,7 +182,7 @@ public class ShardedCounterV2 {
    * Increment datastore property value inside a transaction. If the entity with
    * the provided key does not exist, instead create an entity with the supplied
    * initial property value.
-   *
+   * 
    * @param key the entity key to update or create
    * @param prop the property name to be incremented
    * @param increment the amount by which to increment
